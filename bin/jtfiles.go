77d2a4b8e57ea1be180fd05af9c8cb9f295ae891
Jose Tejada <jotego@gmail.com>
2021-12-27 08:00:40 +0100

    qip output

--- bin/jtfiles.go
+++ bin/jtfiles.go
@@ -6,7 +6,9 @@ import (
     "os"
     "strings"
     "flag"
+    "sort"
     "io/ioutil"
+    "path/filepath"
     "gopkg.in/yaml.v2"
 )
 
@@ -39,9 +41,12 @@ type JTFiles struct {
 
 type Args struct {
 	Corename string
+	Rel bool
+	Format string
 }
 
 var parsed []string
+var CWD string
 
 func parse_args( args *Args ) {
 	flag.Usage = func() {
@@ -50,6 +55,8 @@ func parse_args( args *Args ) {
 		os.Exit(0)
 	}
 	flag.StringVar(&args.Corename,"core","","core name")
+	flag.StringVar(&args.Format,"f","qip","Output format. Valid values: qip, sim")
+	flag.BoolVar(&args.Rel,"rel",false,"Output relative paths")
 	flag.Parse()
 	if len(args.Corename)==0 {
 		log.Fatal("JTFILES: You must specify the core name with argument -core")
@@ -147,32 +154,110 @@ func parse_yaml( filename string, files *JTFiles ) {
 	}
 }
 
-func dump_filelist( fl []FileList, origin Origin ) {
+func make_path( path, filename string, rel bool ) (item string) {
+	var err error
+	oldpath := filepath.Join(path,filename)
+	if rel {
+		item, err = filepath.Rel(CWD,oldpath)
+	} else {
+		item = filepath.Clean(oldpath)
+	}
+	if err != nil {
+		log.Fatalf("Cannot parse path to %s\n",oldpath)
+	}
+	return item
+}
+
+func dump_filelist( fl []FileList, all *[]string, origin Origin, rel bool ) {
 	for _,each := range(fl) {
 		var path string
 		switch( origin ) {
-			case GAME: path=os.Getenv("CORES")+"/"+each.From+"/hdl/"
-			case FRAME: path=os.Getenv("JTFRAME")+"/hdl/"+each.From+"/"
-			default: path=os.Getenv("JTROOT")+"/"
+			case GAME: path=filepath.Join(os.Getenv("CORES"),each.From,"hdl")
+			case FRAME: path=filepath.Join(os.Getenv("JTFRAME"),"hdl",each.From)
+			case MODULE: path=filepath.Join(os.Getenv("MODULES"),each.From)
+			default: path=os.Getenv("JTROOT")
 		}
 		for _,each := range(each.Get) {
 			if len(each)>0 {
-				fmt.Println(path+each)
+				*all = append( *all, make_path(path,each,rel) )
 			}
 		}
 	}
 }
 
-func dump_qip( files JTFiles ) {
-	dump_filelist( files.Game, GAME )
-	dump_filelist( files.JTFrame, FRAME )
+func dump_jtmodules( mods []JTModule, all *[]string, rel bool ) {
+	modpath := os.Getenv("MODULES")
+	if mods == nil {
+		return
+	}
+	for _,each := range(mods) {
+		if len(each.Name)>0 {
+			lower := strings.ToLower(each.Name)
+			lower = filepath.Join(lower,"hdl",lower+".qip")
+			*all = append( *all, make_path(modpath,lower,rel) )
+		}
+	}
+}
+
+func collect_files( files JTFiles, rel bool ) []string {
+	all := make([]string,0)
+	dump_filelist( files.Game, &all, GAME, rel )
+	dump_filelist( files.JTFrame, &all, FRAME, rel )
+	dump_jtmodules( files.Modules.JT, &all, rel )
+	dump_filelist( files.Modules.Other, &all, MODULE, rel )
+	sort.Strings(all)
+	if len(all)>0 {
+		// Remove duplicated files
+		uniq := make([]string,0)
+		for _,each := range(all) {
+			if len(uniq)==0 || each != uniq[len(uniq)-1] {
+				uniq = append( uniq, each )
+			}
+		}
+		return uniq
+	} else {
+		return all
+	}
+}
+
+func dump_qip( all []string, rel bool ) {
+	fout, err := os.Create("game.qip")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fout.Close()
+	for _,each := range(all) {
+		filetype := ""
+		switch( filepath.Ext(each) ) {
+			case ".sv": filetype="SYSTEMVERILOG_FILE"
+			case ".vhd": filetype="VHDL_FILE"
+			case ".v": filetype="VERILOG_FILE"
+			case ".qip": filetype="QIP_FILE"
+			default: {
+				log.Fatalf("Unsupported file extension %s in file %s", filepath.Ext(each), each)
+			}
+		}
+		aux := "set_global_assignment -name " + filetype
+		if rel {
+			aux = aux + "[file join $::quartus(qip_path) " + each + "]"
+		} else {
+			aux = aux + " " + each
+		}
+		fmt.Fprintln( fout, aux )
+	}
 }
 
 func main() {
 	var args Args
 	parse_args(&args)
+	CWD,_ = os.Getwd()
 
 	var files JTFiles
 	parse_yaml( get_filename(args), &files )
-	dump_qip(files)
+	all := collect_files(files, args.Rel)
+	dump_qip(all, args.Rel)
+	//switch( args.Format ) {
+	//	case "qip": dump_qip(all)
+	//	default: dump_sim(all)
+	//}
 }
